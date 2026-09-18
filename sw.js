bdc91e811cf5aeb1600f18a8e18877d9f2f60ada
bprobinson50-creator <bp.robinson50@gmail.com>
2026-09-17 21:18:35 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,38 @@
-const CACHE="warborn-v1";const FILES=["./","./index.html","./manifest.webmanifest","./Assets/character-sheet.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "warborn-v2";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./Assets/character-sheet.png",
+  "./Assets/icon-192.png",
+  "./Assets/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
