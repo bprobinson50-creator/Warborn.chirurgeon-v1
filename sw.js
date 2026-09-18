d8f4fca8cff187ae6c49a042480d7c8090933720
bprobinson50-creator <bp.robinson50@gmail.com>
2026-09-17 21:01:40 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE="warborn-v1";const FILES=["./","./index.html","./manifest.webmanifest","./assets/character-sheet.png"];
+const CACHE="warborn-v1";const FILES=["./","./index.html","./manifest.webmanifest","./Assets/character-sheet.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
